f3b56650008420f118480cae85d006ffdfd133c3
Hritwik <hritwiksinghal@outlook.in>
2020-12-27 16:08:17 +0530

    add old selectCH

---
+++ SelectCH_old.m
@@ -0,0 +1,23 @@
+function [CH,Sensors]=SelectCH(Sensors,Model,r)
+    %% Developed by Amin Nazari 
+% 	aminnazari91@gmail.com 
+%	0918 546 2272
+    CH=[];
+    countCHs=0;
+    n=Model.n;
+    
+    for i=1:1:n
+        if(Sensors(i).E>0)          
+            temp_rand=rand;     
+            if (Sensors(i).G<=0)            
+                %Election of Cluster Heads
+                if(temp_rand<= (Model.p/(1-Model.p*mod(r,round(1/Model.p)))))                    
+                    countCHs=countCHs+1; 
+                    CH(countCHs).id=i; %#ok                
+                    Sensors(i).type='C';
+                    Sensors(i).G=round(1/Model.p)-1;        
+                end    
+            end   
+        end 
+    end 
+end
